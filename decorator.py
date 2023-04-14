0b01298a1734a62ee3e5cfcf0cfc3bea4dc932fb
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2023-04-14 00:41:07 -0400

    complete decorator.py

---
+++ decorator.py
@@ -0,0 +1,21 @@
+#decorator
+#decorator decorates a function
+#it takes function defination as a input
+import logging
+import math
+logging.basicConfig(filename='closure.log', level=logging.INFO)
+
+def logger(argFunc):
+    def logFunc(args):
+        print(f'argFunc = {argFunc.__name__}')
+        logging.info(f'Running Funcion: {argFunc.__name__} with Arguments: {args} Success: \'True\' Error: \'True\'')
+        return argFunc(args)
+    return logFunc
+#decorator
+@logger
+def add(args):
+    return math.fsum(args)
+
+print(f'add = {add.__name__}')
+nums = (2,3,5)
+print(f'sum = {add(nums)}')

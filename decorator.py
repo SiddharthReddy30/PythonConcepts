b48c738ee31a04f140472f254ea958e90e009895
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2023-04-18 15:56:17 -0400

    add new function to understand wraps concept

--- decorator.py
+++ decorator.py
@@ -6,6 +6,14 @@ import math
 from functools import wraps
 logging.basicConfig(filename='closure.log', level=logging.INFO)
 
+def printer(argFunc):
+    @wraps(argFunc)
+    def printFunc(args):
+        print(f'myArgs = {args}')
+        return argFunc(args)
+    print(f'printFunc name = {printFunc.__name__}')
+    return printFunc
+
 def logger(argFunc):
     @wraps(argFunc)
     def logFunc(args):
@@ -13,8 +21,10 @@ def logger(argFunc):
         logging.info(f'Running Funcion: {argFunc.__name__} with Arguments: {args} Success: \'True\' Error: \'True\'')
         return argFunc(args)
     return logFunc
+
 #decorator
 @logger
+@printer
 def add(args):
     return math.fsum(args)
 

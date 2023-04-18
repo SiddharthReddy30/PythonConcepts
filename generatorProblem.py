68dd009d12174c5b974087837d1901f933aae33f
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2023-04-18 19:09:06 -0400

    test generator next functionality

--- generatorProblem.py
+++ generatorProblem.py
@@ -6,3 +6,12 @@ def sentenceGen(inputString: str):
 
 for word in sentenceGen('how its going mom'):
     print(word)
+
+mySentence = sentenceGen('this is test for generator')
+
+print(next(mySentence))
+print(next(mySentence))
+print(next(mySentence))
+print(next(mySentence))
+print(next(mySentence))
+print(next(mySentence))

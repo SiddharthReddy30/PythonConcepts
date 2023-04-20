892e45aa379ee6ef911037df788a2f295bdc79a3
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2023-04-20 15:29:33 -0400

    add xfail/skip description

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -1,5 +1,14 @@
 import pytest
 
+# To run specific tests, in a file
+#   1.Substring matching
+#     pytest -k <sub-string>
+#   2.Set markers
+#     pytest -m <marker-name>
+
+#Xfail/skip
+#Xfail - executes test but doesn't care about the output
+#skip - skips the execution of the test.
 @pytest.mark.xfail
 @pytest.mark.greater
 def test_greater():
@@ -15,8 +24,3 @@ def test_greater_equal():
 def test_less():
     num = 100
     assert num < 200
-# To run specific tests, in a file
-# 1.Substring matching
-#  pytest -k <sub-string>
-# 2.Set markers
-#  pytest -m <marker-name>

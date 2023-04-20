61c0002f6cf00112c0c2a0be89d09e9f9b8909ae
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2023-04-20 15:15:15 -0400

    implement Xfail/skip Test

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -1,10 +1,12 @@
 import pytest
 
+@pytest.mark.xfail
 @pytest.mark.greater
 def test_greater():
     num = 100
     assert num > 100
-        
+
+@pytest.mark.skip
 @pytest.mark.greater
 def test_greater_equal():
     num = 100

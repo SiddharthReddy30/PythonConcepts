9bb724c3d03328a2138636a29b491d574643d766
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2023-04-20 03:11:46 -0400

    Execute a subset of test suite

---
+++ tests/compare_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+@pytest.mark.greater
+def test_greater():
+    num = 100
+    assert num > 100
+        
+@pytest.mark.greater
+def test_greater_equal():
+    num = 100
+    assert num >= 100
+
+def test_less():
+    num = 100
+    assert num < 200
+# To run specific tests, in a file
+# 1.Substring matching
+#  pytest -k <sub-string>
+# 2.Set markers
+#  pytest -m <marker-name>
